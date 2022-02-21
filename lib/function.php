57011fcb44f05f3178804ff3bdf23a03b771bf92
Hussain <msh52@njit.edu>
2022-02-21 12:15:46 -0500

    adding stuff2

--- lib/function.php
+++ lib/function.php
@@ -3,7 +3,8 @@
 require_once(__DIR__ . "/db.php");
 
 //require safer_echo.php
-require(__DIR__ . "/safer_echo.php");
+require(__DIR__ . "/safer_echo.php 
+");
 //TODO 2: filter helpers
 
 //TODO 3: User helpers
