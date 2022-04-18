6b18f9ea7db9bd686287759084d5d6bc444e9bed
Hussain <msh52@njit.edu>
2022-04-18 15:35:42 -0400

    adding cart and tables

--- public_html/Project/helpers.js
+++ public_html/Project/helpers.js
@@ -1,4 +1,4 @@
-function flash(message = "", color = "info") {
+function flash (message = "", color = "info") {
     let flash = document.getElementById("flash");
     //create a div (or whatever wrapper we want)
     let outerDiv = document.createElement("div");
@@ -14,25 +14,19 @@ function flash(message = "", color = "info") {
     //add the element to the DOM (if we don't it merely exists in memory)
     flash.appendChild(outerDiv);
 }
-function isValidUsername(username){
+function isValidUsername (username) {
     const pattern = /^[a-z0-9_-]{3,16}$/;
     return pattern.test(username);
 }
-
-function isValidEmail(email){
+function isValidEmail (email) {
     return true;
-
 }
-
-function isValidPassword(password){
-    if (!password){
+function isValidPassword (password) {
+    if (!password) {
         return false;
     }
-    return password.length >=8;
-    
+    return password.length >= 8;
 }
-
-function isEqual(a, b){
-    return a == b;
+function isEqual (a, b) {
+    return a === b;
 }
-
